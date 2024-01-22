3755b3b0cf55749b1ee329cd24410c729f2b433f
Thierry-git <101677802+Thierry-git@users.noreply.github.com>
2024-01-22 13:40:42 -0500

    exo1 v1

--- exo1.py
+++ exo1.py
@@ -1,5 +1,11 @@
 # TODO : Créer un script Python pour saisir et publier des découvertes planétaires sur un blog intergalactique.
 
-planete = input("Entrez le nom de la planete exploree: ")
-date_visite = ...
-description = ...
+planet = input("Entrez le nom de la planete explorée: ")
+date_of_visit = input("Entrez la date de votre visite (JJ/MM/AAAA) :")
+description = input("Décrivez la planète :")
+
+print("Votre exploration a été ajoutée au Journal des Astres!")
+print(32*"-")
+print(f"Titre : Découverte de la planète {planet}")
+print(f"Date : {date_of_visit}")
+print(f"Description : {description}")
