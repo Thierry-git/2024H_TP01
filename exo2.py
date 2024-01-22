6ddd9e4424518b22b52bb20fdd7c6ba516a34d62
Thierry-git <101677802+Thierry-git@users.noreply.github.com>
2024-01-22 17:23:20 -0500

    exos 2,6
    exo 2/6 v1

--- exo2.py
+++ exo2.py
@@ -1,3 +1,11 @@
 # TODO : Calculer les quantités de matériaux nécessaires pour fabriquer un nombre donné de baguettes magiques.
 
-nombre_baguettes = ...
+wand_ingredients = ("piece(s) de bois",
+                    "coeur(s) de creatures magiques",
+                    "ml de vernis")
+wand_ingredient_weights = (3, 1, 10)
+
+wand_qty = int(input("Nombre de baguettes a fabriquer : "))
+print(f"Voici les materiaux requis pour la fabrication de {wand_qty} baguettes magiques:")
+for i in range(len(wand_ingredients)):
+    print(f"- {wand_qty * wand_ingredient_weights[i]} {wand_ingredients[i]}")
--- exo6.py
+++ exo6.py
@@ -1,3 +1,23 @@
 # TODO : Écrire un programme qui demande le pourcentage de charge actuel de la batterie de la Batmobile, calcule le temps restant en minutes en fonction de ce pourcentage, et affiche le résultat au format "XXhYYmin". Assurer la gestion des pourcentages valides et des messages d'erreur appropriés.
 
-niveau_charge = ...
+# Upper bounds of the different ranges, in percentage points.
+# The 0 in the last spot is only accessed at the first step of the computation, via a -1 index
+ranges = (5, 50, 100, 0)
+weights = (5, 2, 1) # In minutes per percentage point in the corresponding range
+
+charge_level = int(input("Entrez le pourcentage de charge actuel de la batterie de la Batmobile : "))
+
+# Invalid charge value
+if charge_level < 0 or 100 < charge_level:
+    print("Erreur : niveau de charge invalide.")
+# Valid charge value
+else:
+    # How much charge is contributed in each of the ranges
+    charge_in_ranges = [ max(min(charge_level, ranges[i]) - ranges[i-1], 0) for i in range(len(ranges)-1) ]
+
+    # Total battery time, in minutes
+    battery_time = sum([charge_in_ranges[i] * weights[i] for i in range(len(ranges)-1)])
+
+    battery_hours, battery_minutes = divmod(battery_time, 60)
+
+    print(f"Temps restant : {battery_hours}h" + str(battery_minutes).zfill(2))
